6a59e90b670f9d24ec123130201e2550cc6f34d2
Jack Bridger <jacksbridger@gmail.com>
2023-01-17 13:11:01 +0000

    delete editscreeninfo

--- components/EditScreenInfo.tsx
+++
@@ -1,6 +0,0 @@
-import * as WebBrowser from "expo-web-browser";
-import { StyleSheet, TouchableOpacity, View } from "react-native";
-
-export default function EditScreenInfo() {
-  return <View></View>;
-}
--- screens/ModalScreen.tsx
+++ screens/ModalScreen.tsx
@@ -1,12 +1,9 @@
 import { StatusBar } from "expo-status-bar";
 import { Platform, StyleSheet, View } from "react-native";
 
-import EditScreenInfo from "../components/EditScreenInfo";
-
 export default function ModalScreen() {
   return (
     <View>
-      <EditScreenInfo path="/screens/ModalScreen.tsx" />
       <StatusBar style={Platform.OS === "ios" ? "light" : "auto"} />
     </View>
   );
