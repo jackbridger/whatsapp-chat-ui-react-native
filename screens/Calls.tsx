efa7a269cd8f155e816c3fb0b7445f28d5d72f89
Jack Bridger <jacksbridger@gmail.com>
2023-01-18 09:58:07 +0000

    removing some styling, checkpoint for article part 1

--- screens/Calls.tsx
+++ screens/Calls.tsx
@@ -14,13 +14,4 @@ const styles = StyleSheet.create({
     alignItems: "center",
     justifyContent: "center",
   },
-  title: {
-    fontSize: 20,
-    fontWeight: "bold",
-  },
-  separator: {
-    marginVertical: 30,
-    height: 1,
-    width: "80%",
-  },
 });
